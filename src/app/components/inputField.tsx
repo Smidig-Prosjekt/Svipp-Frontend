4d21bcf0b015db4b12c7937fc17122a5276de9ae
Miriam F. Haugland <mifo007@student.kristiania.no>
2025-12-02 11:04:04 +0100

    Created InputField component.

--- src/app/components/inputField.tsx
+++ src/app/components/inputField.tsx
@@ -1,7 +1,24 @@
-export default function InputField() {
+import { HTMLInputTypeAttribute } from "react";
+
+/* 
+    How to use: 
+    <InputField title="Passord" onChange={handleChange} type='password' required value={show an existing value here, like pickup and delivery address that is already chosen.} placeholder="Skriv inn ditt passord"></InputField>
+*/
+
+type Props = {
+    title?: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    type: HTMLInputTypeAttribute;
+    required?: boolean;
+    value?: string;
+    placeholder: string;
+}
+
+export default function InputField({ title, onChange, type, required, value, placeholder}: Props) {
     return (
         <>
-        
+            <h1 className="font-medium">{title}</h1>
+            <input onChange={onChange} type={type} required={required} value={value} placeholder={placeholder} className="w-full h-10 border border-gray-400 rounded-sm font-medium pl-4"></input>
         </>
     )
 }
